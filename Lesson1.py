79a8a35b36bf96a3e48a97f2248d1ed34165262a
Al-Zunaina <alzunainaambar0@gmail.com>
2025-08-06 15:45:07 +0530

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,26 @@
+print('my name is Zuniana')
+print(456)
+#comment - self notes
+var1=98
+var2='12678'
+var3=78.34
+var4= False
+#type funtion- tells data type
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+var5= 6
+print(var5)
+#type casting- changing data type of one to another 
+var1=float(var1)
+print(var1)
+var3=int(var3)
+print(var3)
+var2=int(var2)
+print(var2)
+print(type(var2))
+num1= int (input('enter a number'))
+num2= float (input('enter a number'))
+print(num2)
+print(num1+num2)
