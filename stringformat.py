87df22b336be66edfaa8ec6a28ac6ee6cc4a6b44
Al-Zunaina <alzunainaambar0@gmail.com>
2025-08-06 16:27:29 +0530

    Add files via upload

---
+++ stringformat.py
@@ -0,0 +1,16 @@
+# Method-1
+name=input('enter the name/')
+age=input('enter the age/')
+print(name,'is eating cake and',name,'is',age,'years old')
+temperature= input('enter the temperature')
+print('the temperature is', temperature,'degree C')
+weight=input('enter the weight')
+print("Your weight is",weight,'kgs')
+# Method-2 f -string
+print (f'i am {name} and {age} years old')
+icecream=input('enter the ivecream flavour?')
+outlet=input('enter the outlet?')
+print(f'I like {icecream} flavour icecream from {outlet}')
+#Method-3 dot format
+print('I am {} and I am {} years old'.format(name,age))
+
