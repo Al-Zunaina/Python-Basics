ee8c79e0bc69a01d1c7186b1f5f1ec4bba8be602
Al-Zunaina <alzunainaambar0@gmail.com>
2025-08-11 16:28:33 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,23 @@
+num1= int(input('enter the number/'))
+num2= int(input('enter the number/'))
+#arithematic operators
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
+print(num1//num2)
+print(num1**num2)
+#Comparision Relational operator
+print(num1>num2)
+print(num1<num2)
+print(num1<=num2)
+print(num1>=num2)
+print(num1==num2)
+#assignment operator
+able1=56
+print(able1)
+able1+=10
+print(able1)
+able1//=6
+print(able1)
---
+++ string operator.py
@@ -0,0 +1,9 @@
+'''name=input('enter the  first name')
+last= input('enter the last name')
+print(name+" "+last+' is your full name')
+'''
+word='happy '
+num=6
+print(word*num)
+sentence='it was a fun weekend'#membership operator
+print('weekend' in sentence)
