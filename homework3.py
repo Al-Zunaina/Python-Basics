8d8fef45557409e4fc49a9d673a45cbe4ddac30f
Al-Zunaina <alzunainaambar0@gmail.com>
2025-08-13 15:50:00 +0530

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,22 @@
+'''#area of circle
+radius=float(input('Enter the radius'))
+pi=3.14
+print(pi*radius**2)
+num1=int(input('Enter the number'))
+num2=int(input('Enter the number'))
+#remainder and quotient
+print(num1/num2)
+print(num1%num2)
+#square and cube
+var1=6
+print(var1**2)
+print(var1**3)'''
+C=40
+print(str(1.8*C+32)+'F')
+p=3400
+rate=15
+time=3
+si=((p*rate*time)/100)
+print(si)
+print(p+si)
+
