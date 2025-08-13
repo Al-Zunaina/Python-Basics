23b078e70c0f80e50c92d4d6488d5a8bb6b46e9d
Al-Zunaina <alzunainaambar0@gmail.com>
2025-08-13 16:28:04 +0530

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+'''if True:
+    print("party")#indentation
+elif True:
+    print('with')
+else:
+    print("good")'''
+#checking a number
+num=int(input('Enter the number'))
+if num>10 or num==10:
+    print('Greater than 10 or equal to 10')
+else:
+    print('Smaller than 10')
+    
+
+
+
---
+++ evenodd.py
@@ -0,0 +1,14 @@
+num=int(input('Enter the number'))
+if num>0:
+    print('The number is positive')
+elif num==0:
+    print('The number is zero')
+else:
+    print('The number is negative')
+#even or odd
+if num%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+    
+  
