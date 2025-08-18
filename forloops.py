3be79ab74e47f4e2b9cd03a42a225d7cff8a78f0
Al-Zunaina <alzunainaambar0@gmail.com>
2025-08-18 14:28:59 +0530

    Add files via upload

---
+++ forloops.py
@@ -0,0 +1,21 @@
+'''#for loop
+for i in range(8):
+    print('HELLO')
+    print(i)
+#problem 1
+name=input('Enter the name')
+for i in range(15):
+    print(name)
+for i in range(1,11):
+   # print('HELLO')
+    print(i)
+for n in range(1,20,2):
+    print(n)
+for n in range(2,20,2):
+    print(n)
+for n in range(20,-1,2):
+    print(n)'''
+num=int(input('Enter the number'))
+for n in range(num,num*10+1,num):
+    print(n)
+
