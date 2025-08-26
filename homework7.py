831881b842adf0e0618f1a453edd1367dd200e8d
Al-Zunaina <alzunainaambar0@gmail.com>
2025-08-26 13:43:54 +0530

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,20 @@
+'''odd=1
+while odd<=30:
+     if odd%2!=0:
+        print(odd)
+     odd=odd+1
+even=51
+while even>=1:
+     if even%2==0:
+      print(even)
+     even=even-1
+var=1
+while var<=30:
+   print(var)
+   var=var+2'''
+var2=50
+while var2>=1:
+   print(var2)
+   var2=var2-2
+
+        
